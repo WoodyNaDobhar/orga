febe31846f5640c3dd41a730363fefb390607e5c
Woody <woodynadobhar@hotmail.com>
2023-11-02 15:34:47 -0700

    Import Ork 3
    Signed-off-by: Woody <woodynadobhar@hotmail.com>

--- deploy.php
+++ deploy.php
@@ -3,36 +3,84 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'contrib/php-fpm.php';
 require 'contrib/npm.php';
+require 'contrib/rsync.php';
+
+///////////////////////////////////
+// Config
+///////////////////////////////////
 
 set('application', 'Orga');
 set('repository', 'git@github.com:WoodyNaDobhar/orga.git'); // Git Repository
-set('php_fpm_version', '8.2');
 set('ssh_multiplexing', true);  // Speed up deployment
+//set('default_timeout', 1000);
+
+set('rsync_src', function () {
+	return __DIR__; // If your project isn't in the root, you'll need to change this.
+});
+	
+// Configuring the rsync exclusions.
+// You'll want to exclude anything that you don't want on the server.
+add('rsync', [
+		'exclude' => [
+				'.git',
+				'/vendor/',
+				'/node_modules/',
+				'.github',
+				'deploy.php',
+				'/etc/'
+		],
+]);
+
+// Set up a deployer task to copy secrets to the server.
+// Grabs the dotenv file from the github secret
+task('deploy:secrets', function () {
+	file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+	upload('.env', get('deploy_path') . '/shared');
+});
+
+task('deploy:symlink', function () {
+	if (get('use_atomic_symlink')) {
+		run("mv -T {{deploy_path}}/release {{current_path}}");
+	} else {
+		// Atomic symlink does not supported.
+		// Will use simple two steps switch.
+		
+		run("cd {{deploy_path}} && {{bin/symlink}} {{release_path}} release"); // Atomic override symlink.
+		run("cd {{deploy_path}} && rm release"); // Remove release link.
+	}
+});
+	
+///////////////////////////////////
+// Hosts
+///////////////////////////////////
 	
 host('dev')
 	->setHostname('chi108.greengeeks.net') // Hostname or IP address
 	->set('remote_user', 'migra113') // SSH user
 	->set('branch', 'development') // Git branch
-	->set('deploy_path', 'www/orga.interquestonline.com'); // Deploy path
+	->set('deploy_path', 'www/orga'); // Deploy path
 	
+after('deploy:failed', 'deploy:unlock');  // Unlock after failed deploy
+	
+///////////////////////////////////
+// Tasks
+///////////////////////////////////
+
+desc('Start of Deploy the application');
+
 task('deploy', [
 		'deploy:prepare',
+		'rsync',                // Deploy code & built assets
+		'deploy:secrets',       // Deploy secrets
 		'deploy:vendors',
-		'artisan:storage:link',
-		'artisan:view:cache',
-		'artisan:config:cache',
-		'artisan:migrate',
-		'npm:install',
-		'npm:run:prod',
-		'deploy:publish',
-		'php-fpm:reload',
+		'deploy:shared',        //
+		'artisan:storage:link', //
+		'artisan:view:cache',   //
+		'artisan:config:cache', // Laravel specific steps
+		'artisan:migrate',      //
+		'artisan:queue:restart',//
+		'deploy:publish',       //
 ]);
-	
-task('npm:run:prod', function () {
-	cd('{{release_or_current_path}}');
-	run('npm run prod');
-});
-	
-after('deploy:failed', 'deploy:unlock');
+
+desc('End of Deploy the application');
