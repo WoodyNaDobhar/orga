ab21a6363bf81b5dedec1a85d66994a42cded0bf
Woody <woodynadobhar@hotmail.com>
2023-11-06 12:58:17 -0800

    Import Ork 3
    Signed-off-by: Woody <woodynadobhar@hotmail.com>

--- deploy.php
+++ deploy.php
@@ -1,23 +1,49 @@
 <?php
+<?php
+
 namespace Deployer;
 
+// Include the Laravel & rsync recipes
 require 'recipe/laravel.php';
+require 'recipe/rsync.php';
 
-// Config
+set('application', 'ORK4');
+set('ssh_multiplexing', true); // Speed up deployment
 
-set('repository', 'git@github.com:WoodyNaDobhar/orga.git');
+set('rsync_src', function () {
+	return __DIR__; // If your project isn't in the root, you'll need to change this.
+});
+	
+// Configuring the rsync exclusions.
+// You'll want to exclude anything that you don't want on the production server.
+add('rsync', [
+		'exclude' => [
+				'.git',
+				'/.env',
+				'/storage/',
+				'/vendor/',
+				'/node_modules/',
+				'.github',
+				'deploy.php',
+				'/etc/'
+		],
+]);
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+// Set up a deployer task to copy secrets to the server.
+// Grabs the dotenv file from the github secret
+task('deploy:secrets', function () {
+	file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+	upload('.env', get('deploy_path') . '/shared');
+});
 
 // Hosts
 
 host('dev')
-	->setHostname('chi108.greengeeks.net') // Hostname or IP address
-	->set('remote_user', 'migra113') // SSH user
-	->set('branch', 'development') // Git branch
-	->set('deploy_path', '~/www/orga'); // Deploy path
+	->setHostname('chi201.greengeeks.net') // Hostname or IP address
+	->set('remote_user', 'orkorg') // SSH user
+	->set('stage', 'development')
+	->set('branch', 'development')// Git branch
+	->set('deploy_path', '~/www/dev.ork4.org'); // Deploy path
 
 // Hooks
 
